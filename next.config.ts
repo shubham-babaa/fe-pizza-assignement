b4e0c12017ebcdcbfa2915c2ec10f8642b9412ac
shubham-babaa <manshipatel.deep@gmail.com>
2025-05-24 21:22:54 +0530

    Clean: add safe next.config.ts using env variables

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+const nextConfig = {
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
+
+export default nextConfig;
