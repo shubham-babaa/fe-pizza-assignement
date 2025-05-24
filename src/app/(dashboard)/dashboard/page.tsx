fb34a8d3e8a535f858e098a8d47570e3cc426acf
shubham-babaa <manshipatel.deep@gmail.com>
2025-05-24 23:34:10 +0530

    comment removed

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -36,7 +36,7 @@ export default function DashboardPage() {
   ]
 
   return (
-    <div className="p-4">
+    <div className="p-0">
       <div className="max-w-7xl mx-auto">
         {/* Welcome Section */}
         <div className="mb-8">
